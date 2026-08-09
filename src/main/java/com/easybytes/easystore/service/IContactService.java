421daff537520f6dd86955796af762b36f794f74
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-09 12:29:48 +0200

    implement contact service

---
+++ src/main/java/com/easybytes/easystore/service/IContactService.java
@@ -0,0 +1,7 @@
+package com.easybytes.easystore.service;
+
+import com.easybytes.easystore.dto.ContactRequestDto;
+
+public interface IContactService {
+    boolean saveContact(ContactRequestDto contactRequestDto);
+}
