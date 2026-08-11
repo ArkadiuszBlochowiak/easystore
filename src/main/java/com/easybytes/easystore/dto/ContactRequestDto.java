5f29635ad0e4d00ed37584ff503650359d32b107
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-11 12:38:39 +0200

    add validation for contact form

--- src/main/java/com/easybytes/easystore/dto/ContactRequestDto.java
+++ src/main/java/com/easybytes/easystore/dto/ContactRequestDto.java
@@ -1,14 +1,28 @@
 package com.easybytes.easystore.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import java.time.Instant;
 
 @Getter
 @Setter
 public class ContactRequestDto {
+    @NotBlank(message = "Name cannot be empty")
+    @Size(min = 5, max = 30, message = "Name must be between 5 and 30 characters")
     private String name;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email address")
     private String email;
+
+    @NotBlank(message = "Mobile number cannot be empty")
+    @Pattern(regexp = "^\\d{9}$", message = "Mobile number must be 9 digits")
     private String mobileNumber;
+
+    @NotBlank(message = "Message cannot be empty")
+    @Size(min = 5, max = 500, message = "Message must be between 5 and 500 characters")
     private String message;
 }
--- src/main/java/com/easybytes/easystore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/easybytes/easystore/exception/GlobalExceptionHandler.java
@@ -3,11 +3,16 @@ package com.easybytes.easystore.exception;
 import com.easybytes.easystore.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,5 +27,13 @@ public class GlobalExceptionHandler {
         );
 //        return ResponseEntity.status(errorResponseDto.getErrorCode()).body(errorResponseDto);
         return new ResponseEntity<>(errorResponseDto, errorResponseDto.getErrorCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        List<FieldError> fieldErrorList = exception.getBindingResult().getFieldErrors();
+        fieldErrorList.forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
     }
 }
