40069dd02fb8c5cc707e1b387a0cb1ac8bfcb853
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-09-03 15:46:47 +0200

    add path config

--- src/main/java/com/easybytes/easystore/security/EasyStoreSecurityConfig.java
+++ src/main/java/com/easybytes/easystore/security/EasyStoreSecurityConfig.java
@@ -1,25 +1,30 @@
 package com.easybytes.easystore.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.List;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class EasyStoreSecurityConfig {
 
+    private final List<String> publicPaths;
+
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         //options: permitAll, authenticated, denyAll, hasRole
-        http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/api/v1/products/**").permitAll()
-                .requestMatchers("/api/v1/dummy/**").authenticated()
-                .anyRequest().authenticated()
-        );
+        http.authorizeHttpRequests((requests) -> {
+            publicPaths.forEach(path -> requests.requestMatchers(path).permitAll());
+            requests.anyRequest().authenticated();
+        });
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
