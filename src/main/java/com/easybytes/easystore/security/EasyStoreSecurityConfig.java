ed089b877b35b5ce8efc6b0076ecc8385e73a07f
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-09-03 15:16:27 +0200

    add security config

---
+++ src/main/java/com/easybytes/easystore/security/EasyStoreSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.easybytes.easystore.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class EasyStoreSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        //options: permitAll, authenticated, denyAll, hasRole
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/api/v1/products/**").permitAll()
+                .requestMatchers("/api/v1/dummy/**").authenticated()
+                .anyRequest().authenticated()
+        );
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
