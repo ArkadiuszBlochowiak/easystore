b57498b1960aa749c602d60223c995964f5801f4
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-09-07 12:47:52 +0200

    test in memory user details

--- src/main/java/com/easybytes/easystore/security/EasyStoreSecurityConfig.java
+++ src/main/java/com/easybytes/easystore/security/EasyStoreSecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -37,6 +42,19 @@ public class EasyStoreSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user1 = User.builder().username("arek").password("$2a$12$40/S.slw3rHAvQRTyp2MYusX3YWDGKoO0In.Ir9ckvGEJ72HCdiMK").roles("USER").build();
+        var user2 = User.builder().username("admin").password("$2a$12$16/myWatO7sW4ykCWU1pFuDTx6S6rWxRp724RzZ8opavPnryLO1Fe").roles("USER", "ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
