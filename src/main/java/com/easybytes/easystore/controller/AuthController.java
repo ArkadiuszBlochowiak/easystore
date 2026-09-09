a6e4d2c973e0538e0ae4fa35abe1ab14d4398312
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-09-09 18:42:22 +0200

    add user register endpoint

--- src/main/java/com/easybytes/easystore/controller/AuthController.java
+++ src/main/java/com/easybytes/easystore/controller/AuthController.java
@@ -2,8 +2,10 @@ package com.easybytes.easystore.controller;
 
 import com.easybytes.easystore.dto.LoginRequestDto;
 import com.easybytes.easystore.dto.LoginResponseDto;
+import com.easybytes.easystore.dto.RegisterRequestDto;
 import com.easybytes.easystore.dto.UserDto;
 import com.easybytes.easystore.util.JwtUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,17 +14,24 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthenticationManager authenticationManager;
+    private final InMemoryUserDetailsManager inMemoryUserDetailsManager;
+    private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
     @PostMapping("/login")
@@ -45,6 +54,18 @@ public class AuthController {
         } catch (Exception e) {
             return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "An unexpected error occurred");
         }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@Valid RegisterRequestDto registerRequestDto) {
+        inMemoryUserDetailsManager.createUser(new User(
+                registerRequestDto.getEmail(),
+                passwordEncoder.encode(registerRequestDto.getPassword()),
+                List.of(new SimpleGrantedAuthority("USER"))
+        ));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body("Registration successful");
     }
 
     private ResponseEntity<LoginResponseDto> buildErrorResponse(HttpStatus status, String message) {
---
+++ src/main/java/com/easybytes/easystore/dto/RegisterRequestDto.java
@@ -0,0 +1,26 @@
+package com.easybytes.easystore.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequestDto {
+    @NotBlank(message = "Name is required")
+    @Size(min = 5, max = 30, message = "The length of the name should be between 5 and 30 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email address must be a valid value")
+    private String email;
+
+    @NotBlank(message = "Mobile number is required")
+    @Pattern(regexp = "^\\d{9}$", message = "Mobile number must be 9 digits")
+    private String mobileNumber;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 4, max = 20, message = "The length of the password should be between 5 and 20 characters")
+    private String password;
+}
