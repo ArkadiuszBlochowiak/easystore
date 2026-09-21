20df74c85c50cdecaba07d761f675ff43330b981
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-09-21 20:48:18 +0200

    implement order controller

---
+++ src/main/java/com/easybytes/easystore/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.easybytes.easystore.controller;
+
+import com.easybytes.easystore.dto.OrderRequestDto;
+import com.easybytes.easystore.service.IOrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final IOrderService iOrderService;
+
+    @PostMapping
+    public ResponseEntity<String> createOrder(@RequestBody OrderRequestDto requestDto) {
+        iOrderService.createOrder(requestDto);
+        return ResponseEntity.ok("Order created successfully!");
+    }
+}
